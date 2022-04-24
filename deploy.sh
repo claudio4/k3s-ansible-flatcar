2c20d90e962862f261c5ce0c6baaf3a673b08ac2
Claudio Yanes <me@claudio4.com>
2022-04-24 23:10:15 +0100

    Allow to use deploy and rrset sh with any cluster

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-ansible-playbook bootstrap-python.yml -i inventory/my-cluster/hosts.ini
-ansible-playbook site.yml -i inventory/my-cluster/hosts.ini
+if [ -z "$1" ]
+then
+      echo "You need to provide a cluster name with $0 my-cluster-name"
+      exit 1
+fi
+
+ansible-playbook bootstrap-python.yml -i "inventory/$1/hosts.ini"
+ansible-playbook site.yml -i "inventory/$1/hosts.ini"
--- reset.sh
+++ reset.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+if [ -z "$1" ]
+then
+      echo "You need to provide a cluster name with $0 my-cluster-name"
+      exit 1
+fi
 
-ansible-playbook reset.yml -i inventory/my-cluster/hosts.ini
+ansible-playbook reset.yml -i "inventory/$1/hosts.ini"
